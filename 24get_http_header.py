24109d9bb5c36a5499cccc6a8b9f9e71c30f5a6f
lzhaoyang <575563079@qq.com>
2017-01-10 22:22:53 +0800

    提取http报文头

--- 24get_http_header.py
+++ 24get_http_header.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python3
 # encoding:utf-8
 # written by:liuzhaoyang
+# 提取http
+# 报文头
+
+import sys,json
+from django.conf import settings
+from django.conf.urls import url
+from django.http import HttpResponse,HttpResponseNotFound
+from django.core.wsgi import get_wsgi_application
+from wsgiref.simple_server import make_server
+
+def v_index(req):
+    body = []
+    body.append('<pre>')
+    for key,value in req.META.items():
+        body.append(key+' : '+str(value))
+    body.append('</pre>')
+    body = '\n'.join(body)
+    print(body)
+    return HttpResponse(body)
+
+urlpatterns = [
+    url(r'^$',v_index),
+]
+
+
+settings.configure()
+settings.ROOT_URLCONF = sys.modules[__name__]
+settings.DEBUG = True
+settings.MIDDLEWARE_CLASSES = ()
+
+wsgi_app = get_wsgi_application()
+httpd = make_server('0.0.0.0',7000,wsgi_app)
+print('start serving at 7000')
+httpd.serve_forever()
